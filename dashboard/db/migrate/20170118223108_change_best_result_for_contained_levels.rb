b5fc9945fb55b1ec3c281a83ff8f429d732dac20
Ram Kandasamy <ram@code.org>
2017-02-01 18:22:32 -0800

    stash

--- dashboard/db/migrate/20170118223108_change_best_result_for_contained_levels.rb
+++ dashboard/db/migrate/20170118223108_change_best_result_for_contained_levels.rb
@@ -6,9 +6,9 @@ class ChangeBestResultForContainedLevels < ActiveRecord::Migration[5.0]
     unless Rails.env.production?
       # Find all existing UserLevels for contained levels, and update their best
       # result to be CONTAINED_LEVEL_RESULT (101)
-      containers = Level.all.select{|x| !x.contained_levels.empty?}
-      contained_level_ids = containers.map{|x| x.contained_levels[0].id}
-      UserLevel.where(level_id: contained_level_ids).update_all(best_result: CONTAINED_LEVEL_RESULT)
+      # containers = Level.all.select{|x| !x.contained_levels.empty?}
+      # contained_level_ids = containers.map{|x| x.contained_levels[0].id}
+      # UserLevel.where(level_id: contained_level_ids).update_all(best_result: CONTAINED_LEVEL_RESULT)
     end
   end
 end
